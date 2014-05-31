0ff174646545e7383e827b41888ac66a973272e5
Kári Helgason <kthelgason@gmail.com>
2014-05-31 17:24:07 +0000

    A user can now log in and out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
-    def index
-        
-    end
+  # adds params for devise
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  def index
+              
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
+  end
 
 end
