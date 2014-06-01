97bb5e50e812abfbafd2c9d52d7ef261e1b7795a
Kári Helgason <kthelgason@gmail.com>
2014-06-01 13:46:12 +0000

    creating chatrooms now works

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,2 +1,14 @@
 class ChatroomController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def show
+    @room = Chatroom.find(params[:id]) 
+  end
+
+  def create
+    @room = Chatroom.create_with_creator(current_user)
+    redirect_to chatroom_url(@room)
+  end
+
 end
--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,4 +1,14 @@
+require 'securerandom'
+
 class Chatroom < ActiveRecord::Base
-  belongs_to :creator
-  belongs_to :messages
+  belongs_to :user
+  has_many :messages
+
+
+# Class methods
+
+  def self.create_with_creator(creator)
+    random_UUID = SecureRandom.base64
+    creator.chatrooms.create(uniqueURL: random_UUID)
+  end
 end
